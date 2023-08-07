55a6976e2e3f7b2d9a63def2130016299fe2cefc
syusuke9999 <6879863+syusuke9999@users.noreply.github.com>
2023-08-08 05:57:17 +0900

    Modify wsgi.py

--- ea_license/wsgi.py
+++ ea_license/wsgi.py
@@ -11,6 +11,10 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ea_license.settings')
+from whitenoise import WhiteNoise
+
 
 application = get_wsgi_application()
+application = WhiteNoise(application, root='/path/to/static/files')
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ea_license.settings')
