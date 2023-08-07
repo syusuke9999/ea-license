2a51419d43c978475c36603865f9c387191771c5
syusuke9999 <6879863+syusuke9999@users.noreply.github.com>
2023-08-08 06:48:33 +0900

    Change urls.py

--- ea_license/urls.py
+++ ea_license/urls.py
@@ -15,9 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include  # include をインポート
+from django.urls import  include  # include をインポート
+from django.urls import path
+from django.views.generic.base import RedirectView
+
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/admin/')),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),  # apiアプリのurls.pyをインクルード
 ]
